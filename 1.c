f1dded9eb3f54a82c293a590152e1ae94710368a
Matheus Guedes <33043368+matheusguedess@users.noreply.github.com>
2017-11-07 20:08:06 -0200

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(void)
+{
+    setlocale(LC_ALL, "");
+    int x, op;
+
+    printf("ESCOLHA A OPÇÃO\n\n");
+    printf("1-????\n");
+    printf("2-????\n");
+    printf("3-????\n");
+    scanf(" %d", &op);
+
+
+        switch(op)
+        {
+    case 1:
+        printf("????");
+    break;
+    case 2:
+        printf("????");
+    break;
+    case 3:
+        printf("????");
+    break;
+    default:
+        main();
+        }
+
+    getchar();
+    return 0;
+}
